e26905f5c5c821bc4d9b2282e03f06de97175686
Krish Agarwal <krishagarwal@live.com>
2025-11-13 23:56:45 -0500

    update

--- vbench/__init__.py
+++ vbench/__init__.py
@@ -172,8 +172,7 @@ class VBench(object):
         results_dict = {}
         if dimension_list is None:
             dimension_list = self.build_full_dimension_list()
-        if get_rank() == 0:
-            submodules_dict = init_submodules(dimension_list, local=local, read_frame=read_frame)
+        submodules_dict = init_submodules(dimension_list, local=local, read_frame=read_frame)
 
         cur_full_info_path = self.build_full_info_json(videos_path, name, dimension_list, prompt_list, mode=mode, **kwargs)
         
