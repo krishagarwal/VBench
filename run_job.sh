46fa5894c799de663aa0d4c43f8b698245b87b62
Krish Agarwal <krishagarwal@live.com>
2026-01-28 09:46:35 -0500

    update

--- run_job.sh
+++ run_job.sh
@@ -10,8 +10,8 @@
 # kubectl apply -f yaml/wan_fewstep_topk.yaml
 # kubectl apply -f yaml/wan_fewstep_dmd_monarch_fast_framewise.yaml
 # kubectl apply -f yaml/wan_fewstep_dmd_monarch_fast_framewise_reduce2xh.yaml
-# kubectl apply -f yaml/wan_fewstep_dmd_vsa.yaml
+kubectl apply -f yaml/wan_fewstep_dmd_vsa.yaml
 # kubectl apply -f yaml/wan_fewstep_dmd_radial_attn.yaml
-kubectl apply -f yaml/self_forcing_monarch_fast_framewise.yaml
-kubectl apply -f yaml/self_forcing_monarch_slow_framewise.yaml
-kubectl apply -f yaml/wan_fewstep_svg2_0.85.yaml
+# kubectl apply -f yaml/self_forcing_monarch_fast_framewise.yaml
+# kubectl apply -f yaml/self_forcing_monarch_slow_framewise.yaml
+# kubectl apply -f yaml/wan_fewstep_svg2_0.85.yaml
