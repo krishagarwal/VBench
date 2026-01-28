45d5e61e0f9ca33da87def9754c0f6e1db65e672
Krish Agarwal <krishagarwal@live.com>
2026-01-28 18:39:01 -0500

    update

--- run_job.sh
+++ run_job.sh
@@ -2,18 +2,18 @@
 # kubectl apply -f yaml/wan_fewstep_dmd.yaml
 # kubectl apply -f yaml/wan_fewstep_monarch_fast_framewise.yaml
 # kubectl apply -f yaml/wan_fewstep_monarch_fast_framewise_reduce2xh.yaml
-kubectl apply -f yaml/wan_fewstep_monarch_slow_framewise.yaml
-# kubectl apply -f yaml/wan_fewstep_monarch_slow_framewise_reduce2xh.yaml
+# kubectl apply -f yaml/wan_fewstep_monarch_slow_framewise.yaml
+kubectl apply -f yaml/wan_fewstep_monarch_slow_framewise_reduce2xh.yaml
 # kubectl apply -f yaml/wan_fewstep_radial_attn.yaml
 # kubectl apply -f yaml/wan_fewstep_svg.yaml
 # kubectl apply -f yaml/wan_fewstep_svg2.yaml
 # kubectl apply -f yaml/wan_fewstep_topk.yaml
 # kubectl apply -f yaml/wan_fewstep_dmd_monarch_fast_framewise.yaml
 # kubectl apply -f yaml/wan_fewstep_dmd_monarch_fast_framewise_reduce2xh.yaml
-kubectl apply -f yaml/wan_fewstep_dmd_vsa.yaml
+# kubectl apply -f yaml/wan_fewstep_dmd_vsa.yaml
 # kubectl apply -f yaml/wan_fewstep_dmd_radial_attn.yaml
 # kubectl apply -f yaml/self_forcing_monarch_fast_framewise.yaml
-kubectl apply -f yaml/self_forcing_monarch_slow_framewise.yaml
+# kubectl apply -f yaml/self_forcing_monarch_slow_framewise.yaml
 # kubectl apply -f yaml/wan_fewstep_svg2_0.85.yaml
 # kubectl apply -f yaml/wan_fewstep_svg2_0.90.yaml
 # kubectl apply -f yaml/wan_fewstep_svg2_0.95.yaml
