8354142468e452dafb6b267d3a474d4b48583d97
Krish Agarwal <krishagarwal@live.com>
2026-01-28 22:10:11 -0500

    update

--- run_job.sh
+++ run_job.sh
@@ -3,7 +3,7 @@
 # kubectl apply -f yaml/wan_fewstep_monarch_fast_framewise.yaml
 # kubectl apply -f yaml/wan_fewstep_monarch_fast_framewise_reduce2xh.yaml
 # kubectl apply -f yaml/wan_fewstep_monarch_slow_framewise.yaml
-kubectl apply -f yaml/wan_fewstep_monarch_slow_framewise_reduce2xh.yaml
+# kubectl apply -f yaml/wan_fewstep_monarch_slow_framewise_reduce2xh.yaml
 # kubectl apply -f yaml/wan_fewstep_radial_attn.yaml
 # kubectl apply -f yaml/wan_fewstep_svg.yaml
 # kubectl apply -f yaml/wan_fewstep_svg2.yaml
@@ -13,7 +13,7 @@ kubectl apply -f yaml/wan_fewstep_monarch_slow_framewise_reduce2xh.yaml
 # kubectl apply -f yaml/wan_fewstep_dmd_vsa.yaml
 # kubectl apply -f yaml/wan_fewstep_dmd_radial_attn.yaml
 # kubectl apply -f yaml/self_forcing_monarch_fast_framewise.yaml
-# kubectl apply -f yaml/self_forcing_monarch_slow_framewise.yaml
+kubectl apply -f yaml/self_forcing_monarch_slow_framewise.yaml
 # kubectl apply -f yaml/wan_fewstep_svg2_0.85.yaml
 # kubectl apply -f yaml/wan_fewstep_svg2_0.90.yaml
 # kubectl apply -f yaml/wan_fewstep_svg2_0.95.yaml
